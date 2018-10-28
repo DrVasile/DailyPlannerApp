2a9279b92b6a2309d15f4b8cda905ae7eadcf0e0
Vasile Drumea <drumeavasilexd@gmail.com>
2018-10-28 18:30:39 +0200

    Add activity for adding tasks

--- app/src/main/java/com/example/vasile/dailyplanner/MainActivity.java
+++ app/src/main/java/com/example/vasile/dailyplanner/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.vasile.dailyplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.vasile.dailyplanner";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addTask(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void updateTask(View view) {
+
+    }
+
+    public void deleteTask(View view) {
+
     }
 }
