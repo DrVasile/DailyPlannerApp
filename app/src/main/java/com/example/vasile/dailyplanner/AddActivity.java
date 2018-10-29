1746e951c2b196fe0171144e97c2061c9f5cdf27
Vasile Drumea <drumeavasilexd@gmail.com>
2018-10-29 12:24:13 +0200

    Send date

--- app/src/main/java/com/example/vasile/dailyplanner/AddActivity.java
+++ app/src/main/java/com/example/vasile/dailyplanner/AddActivity.java
@@ -1,7 +1,11 @@
 package com.example.vasile.dailyplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        Intent incoming = getIntent();
+        String date = "Task for " + incoming.getStringExtra(MainActivity.DATE);
+        TextView header = (TextView) findViewById(R.id.textView3);
+        header.setText(date);
     }
 }
--- app/src/main/java/com/example/vasile/dailyplanner/MainActivity.java
+++ app/src/main/java/com/example/vasile/dailyplanner/MainActivity.java
@@ -3,24 +3,39 @@ package com.example.vasile.dailyplanner;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "CalendarActivity";
+    public static final String DATE = "Date";
     public static final String EXTRA_MESSAGE = "com.example.vasile.dailyplanner";
+
+    public String date = "1997/07/30";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                date = year + "/" + month + "/" + day;
+            }
+        });
     }
 
     public void addTask(View view) {
         Intent intent = new Intent(this, AddActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(DATE, date);
         startActivity(intent);
     }
 
@@ -30,5 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void deleteTask(View view) {
 
+    }
+
+    public void searchByKey(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
